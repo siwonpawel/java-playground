fc6a4fb5fffb4409ddf47d1fc63c70e9fb0f034f
Pawel Siwon <siwonpawel@gmail.com>
2021-07-18 17:57:39 +0200

    Next excercises from functional java book

--- src/main/java/functional/custom/function/Function.java
+++ src/main/java/functional/custom/function/Function.java
@@ -8,5 +8,13 @@ public interface Function<T, U> {
     static <I, N, M> Function<I, M> compose(Function<N, M> f1, Function<I, N> f2) {
         return arg -> f1.apply(f2.apply(arg));
     }
+
+    static <T, U, V> Function<Function<U, V>, Function<Function<T, U>, Function<T, V>>> higherCompose() {
+        return x -> y -> z -> x.apply(y.apply(z));
+    }
+
+    static <T, U, V> Function<Function<T, U>, Function<Function<U, V>, Function<T,V>>> higherAndThen() {
+        return x -> y -> z -> y.apply(x.apply(z));
+    }
 }
 
--- src/test/java/functional/custom/function/FunctionTest.java
+++ src/test/java/functional/custom/function/FunctionTest.java
@@ -18,7 +18,7 @@ class FunctionTest {
     }
 
     @Test
-    void shouldComposeFuntions() {
+    void shouldComposeFunctions() {
         //given
         Function<Integer, Integer> triple = x -> x * 3;
         Function<Integer, String> toString = String::valueOf;
@@ -29,6 +29,79 @@ class FunctionTest {
 
         //then
         Assertions.assertEquals("30", result);
+    }
+
+    @Test
+    void ex_2_3_writeFunctionToAddTwoNumbers() {
+        //given
+        Function<Integer, Function<Integer, Integer>> add = y -> x -> x + y;
+
+        //when
+        var result = add.apply(4).apply(5);
+
+        //then
+        Assertions.assertEquals(9, result);
+    }
+
+    @Test
+    void ex_2_4_writeFunctionComposedOfSquareAndTriple() {
+        //given
+        Function<Integer, Integer> square = x -> x * x;
+        Function<Integer, Integer> triple = x -> x * x * x;
+        Function<Integer, Integer> compose = x -> Function.compose(square, triple).apply(x);
+
+        //when
+        var result = compose.apply(2);
+
+        //then
+        Assertions.assertEquals(64, result);
+    }
+
+    @Test
+    void ex_2_5_higherCompose() {
+        //given
+        Function<Integer, Integer> twice = x -> 2 * x;
+        Function<Integer, String> toText = String::valueOf;
+
+        //when
+        var result = Function.<Integer, Integer, String>higherCompose()
+                .apply(toText)
+                .apply(twice)
+                .apply(2);
+
+        //then
+        Assertions.assertEquals("4", result);
+    }
+
+    @Test
+    void ex_2_6_higherAndThen() {
+        //given
+        Function<Integer, Integer> twice = x -> 2 * x;
+        Function<Integer, String> toText = String::valueOf;
+
+        //
+        var result = Function.<Integer, Integer, String>higherAndThen()
+                .apply(twice)
+                .apply(toText)
+                .apply(2);
+
+        //
+        Assertions.assertEquals("4", result);
+    }
+
+    @Test
+    void ex_2_7_partialFunction() {
+        //given
+        var stringify = Function.<Double, Double, String>higherCompose()
+                .apply(String::valueOf);
+
+        //when
+        var apply1 = stringify
+                .apply(x -> x * 0.23)
+                .apply(10.0);
+
+        //then
+        Assertions.assertEquals(2.3F, Float.parseFloat(result), 0.0001);
     }
 
 }
